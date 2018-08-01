05968da80a4d56c8081b5557cb4eadf9d08c0c69
mattraydev <mattray.dev@gmail.com>
2018-08-01 18:34:16 +0100

    basic user account login

--- src/Main/mainLogin.java
+++ src/Main/mainLogin.java
@@ -7,27 +7,45 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class mainLogin {
 @Test
 	
 	public void landingPageTest() throws InterruptedException {
+
+		//remove before committing !!!!!!!!!!!!!!!
+		String accountEmail = "";
+		String accountPassword = "";
 		
-		String loginBtn = "//*/div//ul/a[2]";
+		String loginHomeBtn = "//*/div//ul/a[2]";
 		String loginForm = "//*/div[@class='login-form']";
-		
+		String emailInput ="//*/input[@type='email']";
+		String passwordInput = "//*/input[@type='password']";
+		String loginBtn = "//*/input[@type='submit']";
 		
 		System.setProperty("webdriver.gecko.driver", "C:\\geckoDriver\\geckodriver.exe");
 	    WebDriver driver = new FirefoxDriver();
-	    
+	    WebDriverWait wait = new WebDriverWait(driver, 5);
 	    driver.get("https://www.findmyflock.com/");
-	    WebElement header = driver.findElement(By.xpath(loginBtn));
+	    
+	    WebElement header = driver.findElement(By.xpath(loginHomeBtn));
+	    wait.until(ExpectedConditions.elementToBeClickable(header));
 	    assertTrue((header.isDisplayed())); 
 	    header.click();
-	    Thread.sleep(3000);
 	    
 	    WebElement logForm = driver.findElement(By.xpath(loginForm));
 	    assertTrue((logForm.isDisplayed()));
+	    WebElement email = driver.findElement(By.xpath(emailInput));
+	    email.click();
+	    email.sendKeys(accountEmail);
+	    WebElement password = driver.findElement(By.xpath(passwordInput));
+	    password.click();
+	    password.sendKeys(accountPassword);
+	    WebElement submit = driver.findElement(By.xpath(loginBtn));
+	    submit.click();
+	    Thread.sleep(5000);
 	    driver.close();  
 	}
 
